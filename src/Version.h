a84edc54bc7f0cb0a83547a0fa2ad5ac0e18fc76
dengxibo <22745709@qq.com>
2026-09-15 02:45:50 +0800

    Bump version to 3.7.31

--- src/Version.h
+++ src/Version.h
@@ -3,10 +3,10 @@
 
 // CURR_VERSION can be over-written externally
 #ifndef CURR_VERSION
-#define CURR_VERSION 3.7.30
+#define CURR_VERSION 3.7.31
 #endif
 #ifndef CURR_VERSION_COMMA
-#define CURR_VERSION_COMMA 3, 7, 30
+#define CURR_VERSION_COMMA 3, 7, 31
 #endif
 
 // this is sth. like "3.5"
